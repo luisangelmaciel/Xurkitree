0232cea87c3bfdf9e70cd865002afd57fd05c350
luisangelmaciel <89158021+luisangelmaciel@users.noreply.github.com>
2021-08-26 01:04:57 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.body.addEventListener("mousemove", evt => {
+  const mouseX = evt.clientX;
+  const mouseY = evt.clientY;
+
+  gsap.set(".cursor", {
+    x: mouseX,
+    y: mouseY });
+
+
+  gsap.to(".shape", {
+    x: mouseX,
+    y: mouseY,
+    stagger: -0.1 });
+
+});
